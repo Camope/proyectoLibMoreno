004f21bbf5479f8129c04e3b48a7fb4f81adc641
Carlos Moreno <camorpe@gmail.com>
2023-03-16 12:09:37 +0100

    commit inicial

---
+++ src/main/java/es/mde/acing/proyectoLibMoreno/ProyectoLibMorenoApplication.java
@@ -0,0 +1,13 @@
+package es.mde.acing.proyectoLibMoreno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLibMorenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLibMorenoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/mde/acing/proyectoLibMoreno/ProyectoLibMorenoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mde.acing.proyectoLibMoreno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLibMorenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
