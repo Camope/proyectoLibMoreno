28972025b7fa46583f200bc365fb37eed6fbc9eb
Carlos Moreno <camorpe@gmail.com>
2023-03-16 13:08:17 +0100

    commit dos

--- src/main/java/es/mde/acing/proyectoLibMoreno/ProyectoLibMorenoApplication.java
+++ src/main/java/es/mde/acing/proyectoLibMoreno/ProyectoLibMorenoApplication.java
@@ -1,13 +1,9 @@
 package es.mde.acing.proyectoLibMoreno;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class ProyectoLibMorenoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProyectoLibMorenoApplication.class, args);
 	}
 
 }
---
+++ src/main/java/es/mde/acing/proyectoLibMoreno/utils/PruebaLibreria.java
@@ -0,0 +1,8 @@
+package es.mde.acing.proyectoLibMoreno.utils;
+
+public class PruebaLibreria {
+
+	public static void imprime() {
+		System.out.println("Hola librería!!");
+	}
+}
